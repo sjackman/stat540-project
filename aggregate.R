0b73d4d126a331450115cfaff0a54b32e476c971
Nick Fishbane <nickfishbane@gmail.com>
2013-03-14 01:28:25 -0700

    Changes for clean summarization and the addition of M files

--- aggregate.R
+++ aggregate.R
@@ -1,42 +1,59 @@
 # Aggregate the beta values of the probes for each CpG island.
+# Input: 1.) normalized beta-value data sets
+#        2.) metadata
+# Output: 1.) metadata with groups column
+#         2.) beta-value AND M-value data sets with CPGI attached
+#         3.) beta-value AND M-value CPGI-level data (mean)
 
+
+library(IlluminaHumanMethylation450k.db)
+
+### Load in (EDIT PATH):
+load('Data/All_3_metasets.Rdata')
+load('Data/All_3_sets.Rdata')
+
+dataList <- list(ALL = ALL.dat, APL = APL.dat, CTRL = CTRL.dat)
 #load('~/UBC Stats/STAT540/Group Project/Data/GSE42865_matrix.R')
+
+####### Extracting and cleaning map of probe ID to CpG islands
 cpginame <- as.data.frame(IlluminaHumanMethylation450kCPGINAME)
 colnames(cpginame) <- c('Probe_ID', 'cpginame')
-rownames(cpginame) <- cpginame$Probe_ID
 cpginame$cpginame <- factor(cpginame$cpginame)
-CTRL.cpginame <- merge(CTRL.dat, cpginame, by='row.names')
-sampleNames <- colnames(CTRL.dat)
-CTRL.cpgislands <- simplify2array(by(
-	CTRL.cpginame[,sampleNames],
-	list(CTRL.cpginame$cpginame),
-	colMeans))
 
-#load('~/UBC Stats/STAT540/Group Project/Data/GSE39141_matrix.R')
-ALL.cpginame <- merge(ALL.dat, cpginame, by='row.names')
-ALL.cpgislands <- simplify2array(by(
-	ALL.cpginame[,colnames(ALL.dat)],
-	list(ALL.cpginame$cpginame),
-	colMeans))
+#### There are 309,465 probes (out of total 485,577) in 27,176 islands
 
-# Combine the CTRL and ALL data sets.
-CTRL.meta$Group <- 'CTRL'
-design <- data.frame(
-	Group = relevel(factor(c(CTRL.meta$Group, ALL.meta$Group)), 'CTRL'),
-	row.names = c(row.names(CTRL.meta), row.names(ALL.meta)))
-data <- rbind(CTRL.cpgislands, ALL.cpgislands)
 
-# Fit a linear model.
-library(limma)
-mm <- model.matrix(~Group, design)
-fit <- eBayes(lmFit(t(data), mm))
-tt <- topTable(fit, coef='GroupALL', n=12)
+##### First restrict all data sets to probes in mapping:
+cpgi.probes.Beta <- lapply(dataList, function(x) x[cpginame$Probe_ID,])
 
-# Reshape the data.
-library(reshape2)
-tall <- melt(cbind(design, data), id.vars=colnames(design),
-	variable.name = 'probe', value.name = 'beta')
+##### Create M values:
+cpgi.probes.M <- lapply(cpgi.probes.Beta, logit)
 
-# Plot the hits.
-library(lattice)
-stripplot(beta ~ Group | probe, tall, subset = probe %in% tt$ID, auto.key=T)
+#### Tack on to data in both lists: CPGI as a factor
+cpgi.probes.Beta <- lapply(cpgi.probes.Beta, function(x) 
+  cbind(x, cpgi = cpginame$cpginame))
+cpgi.probes.M <- lapply(cpgi.probes.M, function(x) 
+  cbind(x, cpgi = cpginame$cpginame))
+
+#### Then aggregate both sets by island --> means of both betas and Ms
+#### Note: avoid NA's in mean calculation
+
+cpgi.Beta <- lapply(cpgi.probes.Beta, function(x) 
+  simplify2array(by(
+    x[,-ncol(x)], list(x$cpgi), function(y) colMeans(y, na.rm=T))))
+
+cpgi.M <- lapply(cpgi.probes.M, function(x) 
+  simplify2array(
+    by(x[,-ncol(x)], list(x$cpgi), function(y) colMeans(y, na.rm=T))))
+
+#### Transpose: (want features in rows for most packages)
+cpgi.Beta <- lapply(cpgi.Beta, t)
+cpgi.M <- lapply(cpgi.M, t)
+ 
+
+##### Save these data sets:
+
+########### Edit the path!!!!!!!!!!!!!!!!!!!
+save(cpgi.probes.Beta, file = 'Data/CPGI2Probe_betaList.Rdata')
+save(cpgi.probes.M, file = 'Data/CPGI_MList.Rdata')
+
