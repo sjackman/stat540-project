e8cd98796065c84c0f255818224916f7978f68eb
Nick Fishbane <nickfishbane@gmail.com>
2013-03-21 17:12:25 -0700

    Aggregates normalized and filtered data sets: Edited probe selection in mapping and M-value transformation

--- aggregate.R
+++ aggregate.R
@@ -6,10 +6,14 @@
 
 setwd('~/UBC Stats/STAT540/Group Project/')
 library(IlluminaHumanMethylation450k.db)
+library(plyr)
+library(gtools)
+library(lattice)
+library(reshape2)
 
 ### Load in (EDIT PATH):
 load('Data/All_3_metasets.Rdata')
-load('Data/All_3_sets.Rdata')
+load('Data/All_3_sets_normAndFilt.Rdata')
 
 dataList <- list(ALL = ALL.dat, APL = APL.dat, CTRL = CTRL.dat)
 #load('~/UBC Stats/STAT540/Group Project/Data/GSE42865_matrix.R')
@@ -21,20 +25,61 @@ cpginame$cpginame <- factor(cpginame$cpginame)
 
 #### There are 309,465 probes (out of total 485,577) in 27,176 islands
 
+### Not all probes that map passed through the previous filter:
+table(cpginame$Probe_ID %in% rownames(dataList$ALL))
+# FALSE   TRUE 
+# 19933 289532
 
-##### First restrict all data sets to probes in mapping:
+### Restrict mappings to exclude filtered-out probes
+cpginame <- subset(cpginame, Probe_ID %in% rownames(dataList$ALL))
+
+##### Restrict all data sets to probes in mapping:
 cpgi.probes.Beta <- lapply(dataList, function(x) x[cpginame$Probe_ID,])
 
-##### Create M values
-##### Add 0.001 to avoid getting -Inf. Maximum of any array is 0.9981:
 
-cpgi.probes.M <- lapply(cpgi.probes.Beta, function(x) logit(x + 0.001))
+###############################################
+##### M-value transformation
+###############################################
+
+#### Ranges:
+(ranges <- ldply(cpgi.probes.Beta, function(x) range(x, na.rm = T)))
+#    .id           V1        V2
+# 1  ALL 2.123553e-14 0.9999967
+# 2  APL 1.292368e-02 0.9906188
+# 3 CTRL 3.266047e-14 0.9994150
+names(ranges) <- c('Set', 'Min', 'Max')
+rownames(ranges) <- ranges$Set; ranges$Set <- NULL
+ranges <- as.matrix(ranges)
+
+#### Check out:
+logit(ranges)
+logit(ranges + 1e-6) ## This one seems reasonable! (somewhat symmetric on the tails)
+### Add epsilon to Beta-values before transforming to M-values:
+
+cpgi.probes.M <- lapply(cpgi.probes.Beta, function(x) logit(x + 1e-6))
+
+#### Explore a bit:
+whole.M <- with(cpgi.probes.M, cbind(ALL, APL, CTRL))
+whole.M.tall <- melt(t(whole.M), value.name = 'M', varnames = c('Sample', 'Probe_ID'))
+png('Figures/Normalized_M_CpG_allSamps.png', width = 2000, height = 400)
+par(mar = c(8, 4, 4, 2))
+bwplot(M~Sample, data = whole.M.tall, panel = panel.violin,
+       scales = list(x = list(rot = 90)), xlab = 'Sample', ylab = 'M values',
+       main = 'Distribution of M-values from normalized and filtered Beta-values')
+dev.off()
+rm(whole.M.tall)
+
+####################################################
+####  Aggregation CpG --> Island
+####################################################
 
 #### Tack on to data in both lists: CPGI as a factor
+
 cpgi.probes.Beta <- lapply(cpgi.probes.Beta, function(x) 
   cbind(x, cpgi = cpginame$cpginame))
 cpgi.probes.M <- lapply(cpgi.probes.M, function(x) 
   cbind(x, cpgi = cpginame$cpginame))
+
 
 #### Then aggregate both sets by island --> means of both betas and Ms
 #### Note: avoid NA's in mean calculation
@@ -50,7 +95,6 @@ cpgi.M <- lapply(cpgi.probes.M, function(x)
 #### Transpose: (want features in rows for most packages)
 cpgi.Beta <- lapply(cpgi.Beta, t)
 cpgi.M <- lapply(cpgi.M, t)
- 
 
 ##### Save these data sets:
 
