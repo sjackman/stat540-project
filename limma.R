70fce3c0d371e5d078f5904335317d7621853c0d
Shaun Jackman <sjackman@gmail.com>
2013-03-29 01:43:39 -0700

    Convert the coordinates of CpGI to gene names

--- limma.R
+++ limma.R
@@ -18,11 +18,44 @@ design <- data.frame(
 # Fit a linear model.
 mm <- model.matrix(~Group, design)
 fit <- eBayes(lmFit(t(data), mm))
-tt <- topTable(fit, coef='GroupALL', n=12)
+tt <- topTable(fit, coef='GroupALL', number=Inf, p.value=1e-10)
 
 # Reshape the data.
 tall <- melt(cbind(design, data), id.vars=colnames(design),
   variable.name = 'probe', value.name = 'M')
 
 # Plot the hits.
-stripplot(M ~ Group | probe, tall, subset = probe %in% tt$ID, auto.key=T)
+stripplot(M ~ Group | probe, tall,
+	subset = probe %in% tt[1:12,'ID'], auto.key=T)
+
+# Get the coordinates of genes.
+library(biomaRt)
+library(GenomicRanges)
+genes <- subset(subset = hgnc_symbol != '',
+	getBM(
+		c('hgnc_symbol', 'ensembl_gene_id', 'chromosome_name', 'start_position', 'end_position'),
+		mart=useMart('ensembl', 'hsapiens_gene_ensembl')))
+genes.gr <- GRanges(
+	seqnames = Rle(genes$chromosome_name),
+	ranges = IRanges(start = genes$start_position, end = genes$end_position),
+	names = genes$hgnc_symbol)
+
+# Convert the coordinates of CpG islands to gene names.
+cpgi <- data.frame(t(simplify2array(strsplit(tt$ID, '[:-]'))),
+	row.names=tt$ID,
+	stringsAsFactors=FALSE)
+colnames(cpgi) <- c('chr', 'start', 'end')
+cpgi$chr <- factor(sub('chr', '', cpgi$chr))
+cpgi$start <- as.numeric(cpgi$start)
+cpgi$end <- as.numeric(cpgi$end)
+cpgi.gr <- GRanges(
+	seqnames = Rle(cpgi$chr),
+	ranges = IRanges(start=cpgi$start, end=cpgi$end),
+	names = rownames(cpgi))
+overlaps <- findOverlaps(cpgi.gr, genes.gr)
+geneset <- genes[subjectHits(overlaps), 'hgnc_symbol']
+
+# Write the gene set to a file.
+fd <- file('Data/limma-geneset.txt')
+writeLines(geneset, fd)
+close(fd)
