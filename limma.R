cab396f67fdd52fcf2461b7917e21d236d544fbd
Shaun Jackman <sjackman@gmail.com>
2013-03-29 00:01:33 -0700

    Identify with limma diff. methylated CpG islands

---
+++ limma.R
@@ -0,0 +1,28 @@
+# Identify differentially methylated CpG islands using limma
+
+library(lattice)
+library(limma)
+library(reshape2)
+
+# Load the CpG island M values.
+load('Data/CPGI_MList.Rdata')
+
+# Combine the CTRL and ALL data sets.
+data <- rbind(
+	do.call(cbind, cpgi.M[['CTRL']]),
+	do.call(cbind, cpgi.M[['ALL']]))
+design <- data.frame(
+	Group=relevel(ref='HBC', factor(substr(rownames(data), 1, 3))),
+	row.names=rownames(data))
+
+# Fit a linear model.
+mm <- model.matrix(~Group, design)
+fit <- eBayes(lmFit(t(data), mm))
+tt <- topTable(fit, coef='GroupALL', n=12)
+
+# Reshape the data.
+tall <- melt(cbind(design, data), id.vars=colnames(design),
+  variable.name = 'probe', value.name = 'M')
+
+# Plot the hits.
+stripplot(M ~ Group | probe, tall, subset = probe %in% tt$ID, auto.key=T)
